760390c32f697dfd62982823143cc6f3b3bb6ecf
ignatius22 <igee54077@gmail.com>
2021-02-03 11:53:09 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://homepointrdeep.herokuapp.com'
+    origins 'https://enigmatic-dusk-17553.herokuapp.com'
     resource '*', headers: :any, methods: %i[get post put patch delete options head],
                   credentials: true
   end
